01f1d92921df5cecc69933460fe44402d6ebe0c1
Cristian Fabiano Eisenhut <cristian.eisenhut@compasso.com.br>
2023-07-15 13:00:22 -0300

    loja de roupas

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-const personagens = document.querySelectorAll('.personagem');
+const produtos = document.querySelectorAll('.produto');
 
-personagens.forEach((personagem)=>{
-    personagem.addEventListener('mouseenter', () => {
+produtos.forEach((produto)=>{
+    produto.addEventListener('mouseenter', () => {
 
         if(window.innerWidth < 450){
             window.screenTop({top:0 , behavior: 'smooth'});
         }
         
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
-        personagem.classList.add('selecionado');
+        const produtoSelecionado = document.querySelector('.selecionado');
+        produtoSelecionado.classList.remove('selecionado');
+        produto.classList.add('selecionado');
 
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-        const idPersonagem = personagem.attributes.id.value;       
-        imagemPersonagemGrande.src = `../img/card-${idPersonagem}.png`;
+        const imagemprodutoGrande = document.querySelector('.produto-grande');
+        const idproduto = produto.attributes.id.value;       
+        imagemprodutoGrande.src = `../img/card-${idproduto}.png`;
 
-        const nomePersonagem = document.getElementById('nome-personagem');
-        nomePersonagem.innerText = personagem.getAttribute('data-name');
+        const nomeproduto = document.getElementById('nome-produto');
+        nomeproduto.innerText = produto.getAttribute('data-name');
 
-        const descricaoPersonagem = document.getElementById('descricao-personagem');
-        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+        const descricaoproduto = document.getElementById('descricao-produto');
+        descricaoproduto.innerText = produto.getAttribute('data-description');
 
 
     })
